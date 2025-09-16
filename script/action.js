4c32db34503d5e33cb63744fb3ecf57ddc066c37
Red Swan <redswan@outlook.com>
2025-09-16 15:32:41 -0600

    Add minimal Bulma work
    This commit works within bulma to have a left side right side
    that inserts the right side with javascript actions.
    
    It's clunky and ugly but a good starting point.

---
+++ script/action.js
@@ -0,0 +1,15 @@
+
+
+const content = document.getElementById('content');
+
+
+
+function showAbout() {
+  content.innerHTML = "Hello there";
+}
+
+function showPhilosophy() {
+  content.innerHTML = "General Kenobi";
+}
+
+
